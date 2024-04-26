167662b97913045f82cd62834b23e322100f5338
Md Zahid Hasan <hasanmd1@fit.cvut.cz>
2024-04-26 04:20:23 +0200

    FrontEnd Setup

--- LetsOrder/backend/src/index.ts
+++ LetsOrder/backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import "dotenv/config";
 
@@ -6,6 +6,10 @@ const app = express()
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: Rereq, res) => {
+app.get("/test", async (req: Request, res : Response) => {
+    res.json({ message: "Hello World" });
+});
 
-})
+app.listen(7000, () => {
+    console.log("Server is running on port 7000")
+});
