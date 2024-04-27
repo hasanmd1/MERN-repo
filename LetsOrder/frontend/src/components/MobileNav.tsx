8928abe4cb958255cfad9d1d355df35ce4739ed6
Md Zahid Hasan <hasanmd1@fit.cvut.cz>
2024-04-27 13:08:51 +0200

    User authentication and UI changes fixed with shadcn-ui

--- LetsOrder/frontend/src/components/MobileNav.tsx
+++ LetsOrder/frontend/src/components/MobileNav.tsx
@@ -1,9 +1,12 @@
 import {Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger} from "@/components/ui/sheet.tsx";
-import {Menu} from "lucide-react";
+import {CircleUserRound, Menu} from "lucide-react";
 import {Separator} from "@/components/ui/separator.tsx";
 import {Button} from "@/components/ui/button.tsx";
+import {useAuth0} from "@auth0/auth0-react";
+import MobileNavLinks from "@/components/MobileNavLinks.tsx";
 
 const MobileNav = () => {
+    const {isAuthenticated, loginWithRedirect, user} = useAuth0();
     return (
         <Sheet>
             <SheetTrigger>
@@ -11,14 +14,33 @@ const MobileNav = () => {
             </SheetTrigger>
             <SheetContent className="space-y-3 padding-x-5">
                 <SheetHeader>
-                    <SheetTitle>Get Started With Us!</SheetTitle>
+                    <SheetTitle>
+                        {isAuthenticated ? (
+                            <span className="flex items-center font-bold gap-2">
+                                <CircleUserRound className="text-orange-500"/>
+                                {user?.email}
+                            </span>
+                        ) : (
+                            <span>Welcome to our platform!</span>
+                        )}
+                    </SheetTitle>
                 </SheetHeader>
 
                 <Separator/>
 
-                <SheetDescription className="flex">
-                    <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
-                </SheetDescription>
+                <SheetDescription className="flex flex-col gap-4">
+                    {isAuthenticated ? (
+                        <MobileNavLinks/>
+                    ) : (
+                        <Button
+                            className="flex-1 font-bold bg-orange-500"
+                            onClick={async () => await loginWithRedirect()}
+                        >
+                            Log In
+                        </Button>
+
+                    )}
+                    </SheetDescription>
             </SheetContent>
         </Sheet>
     )
